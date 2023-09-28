6a58fc056382fc166a60244341898c3738011db4
thiago-braga <thiago.braga@wyntech.inf.br>
2023-09-28 08:28:46 -0300

    inserindo assets no service-worker

--- service-worker.js
+++ service-worker.js
@@ -9,6 +9,17 @@ self.addEventListener("install", function (event) {
         "styles/css/style.css",
         "scripts/script.js",
         "assets/avatar2.png",
+        "assets/languages/c--4.svg",
+        "assets/languages/css.svg",
+        "assets/languages/figma.svg",
+        "assets/languages/html.svg",
+        "assets/languages/javascript.svg",
+        "assets/languages/next-js.svg",
+        "assets/languages/react-2.svg",
+        "assets/languages/React-icon.svg",
+        "assets/languages/react-native-1.svg",
+        "assets/languages/sass.svg",
+        "assets/languages/tailwindcss.svg",
       ]);
     })
   );
