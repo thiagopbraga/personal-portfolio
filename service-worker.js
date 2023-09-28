f6b4e3ee7d3a4ed350e55dcb3d95016601c2a0a0
thiago-braga <thiago.braga@wyntech.inf.br>
2023-09-28 01:09:38 -0300

    transformando portifólio em PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+// service-worker.js
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("cache-v1").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "styles/css/style.css",
+        "scripts/script.js",
+        "assets/avatar2.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
