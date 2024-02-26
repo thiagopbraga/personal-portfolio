2d439c601c9c270d536845aeba70c69bdd97941d
Thiago Pinto Braga <thiago.braga@wyntech.inf.br>
2024-02-26 16:04:38 -0300

    Att site

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@
 const initLightMode = () => {
   document
     .querySelector('input[type="checkbox"]')
-    .addEventListener("change", () => {
-      document.body.classList.toggle("light-mode");
+    .addEventListener('change', () => {
+      document.body.classList.toggle('light-mode');
     });
 };
 
@@ -13,43 +13,43 @@ initLightMode();
 // Open Menu
 
 const initOpenMenu = () => {
-  const menuBtn = document.querySelector("header .menu i");
-  const closeBtn = document.querySelector("header .menu i:nth-child(2)");
-  const menu = document.querySelector(".menuOpen");
-  const header = document.querySelector("header");
+  const menuBtn = document.querySelector('header .menu i');
+  const closeBtn = document.querySelector('header .menu i:nth-child(2)');
+  const menu = document.querySelector('.menuOpen');
+  const header = document.querySelector('header');
 
   const close = () => {
-    menu.classList.add("hidden");
-    menuBtn.classList.remove("hidden");
-    closeBtn.classList.add("hidden");
-    document.body.style.overflow = "visible";
-    header.classList.remove("open");
+    menu.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
+    closeBtn.classList.add('hidden');
+    document.body.style.overflow = 'visible';
+    header.classList.remove('open');
   };
 
   const open = () => {
-    menu.classList.remove("hidden");
-    menuBtn.classList.add("hidden");
-    closeBtn.classList.remove("hidden");
-    document.body.style.overflow = "hidden";
-    header.classList.add("open");
+    menu.classList.remove('hidden');
+    menuBtn.classList.add('hidden');
+    closeBtn.classList.remove('hidden');
+    document.body.style.overflow = 'hidden';
+    header.classList.add('open');
   };
 
   // Open with Btn
-  menuBtn.addEventListener("click", open);
+  menuBtn.addEventListener('click', open);
 
   // Close with Btn
-  closeBtn.addEventListener("click", close);
+  closeBtn.addEventListener('click', close);
 
   // Close with Esc
-  document.addEventListener("keydown", (event) =>
-    event.key === "Escape" ? close() : ""
+  document.addEventListener('keydown', (event) =>
+    event.key === 'Escape' ? close() : ''
   );
 
   // Close with Links
-  const links = document.querySelectorAll("nav a");
+  const links = document.querySelectorAll('nav a');
 
   links.forEach((link) => {
-    link.addEventListener("click", close);
+    link.addEventListener('click', close);
   });
 };
 
@@ -58,7 +58,7 @@ initOpenMenu();
 // Scroll Animation
 
 const initAnimationScroll = () => {
-  const sections = document.querySelectorAll(".js-section");
+  const sections = document.querySelectorAll('.js-section');
 
   const windowHalfSize = window.innerHeight * 0.5;
 
@@ -69,14 +69,14 @@ const initAnimationScroll = () => {
       const isSectionVisible = sectionTop - windowHalfSize < 0;
 
       if (isSectionVisible) {
-        item.classList.add("active");
+        item.classList.add('active');
       }
     });
   };
 
   animateScroll();
 
-  window.addEventListener("scroll", animateScroll);
+  window.addEventListener('scroll', animateScroll);
 };
 
 initAnimationScroll();
@@ -84,12 +84,12 @@ initAnimationScroll();
 // Scroll Smooth
 
 const initScrollSmooth = () => {
-  const linksInternos = document.querySelectorAll("nav a");
+  const linksInternos = document.querySelectorAll('nav a');
 
   linksInternos.forEach((item) => {
     const scrollToSection = (event) => {
       event.preventDefault();
-      const href = event.currentTarget.getAttribute("href");
+      const href = event.currentTarget.getAttribute('href');
       const section = document.querySelector(href);
 
       window.scrollTo({
@@ -97,7 +97,7 @@ const initScrollSmooth = () => {
       });
     };
 
-    item.addEventListener("click", scrollToSection);
+    item.addEventListener('click', scrollToSection);
   });
 };
 
@@ -106,31 +106,31 @@ initScrollSmooth();
 // Typing Animation
 
 const initTypingAnimation = () => {
-  const title = document.querySelector("#sobre .banner h1");
-  const span = document.querySelector("#sobre .banner span");
-  const paragraph = document.querySelector("#sobre .banner p");
+  const title = document.querySelector('#sobre .banner h1');
+  const span = document.querySelector('#sobre .banner span');
+  const paragraph = document.querySelector('#sobre .banner p');
 
   const typingAnimation = (element) => {
     if (element == title) {
-      element.innerHTML = "Olá, eu sou o ";
-      const textToArray = element.innerHTML.split("");
-      element.innerHTML = "";
+      element.innerHTML = 'Olá, eu sou o ';
+      const textToArray = element.innerHTML.split('');
+      element.innerHTML = '';
 
       textToArray.forEach((item, index) => {
         setTimeout(() => (element.innerHTML += item), 120 * index);
       });
     } else if (element == span) {
-      element.innerHTML = "Thiago Braga";
-      const textToArray = element.innerHTML.split("");
-      element.innerHTML = "";
+      element.innerHTML = 'Thiago Braga';
+      const textToArray = element.innerHTML.split('');
+      element.innerHTML = '';
 
       textToArray.forEach((item, index) => {
         setTimeout(() => (element.innerHTML += item), 150 * index);
       });
     } else {
-      element.innerHTML = "Desenvolvedor Web e Mobile";
-      const textToArray = element.innerHTML.split("");
-      element.innerHTML = "";
+      element.innerHTML = 'Desenvolvedor Web e Mobile';
+      const textToArray = element.innerHTML.split('');
+      element.innerHTML = '';
 
       textToArray.forEach((item, index) => {
         setTimeout(() => (element.innerHTML += item), 75 * index);
